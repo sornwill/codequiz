9603cb64451f169232083457ce4111ad60a5c187
Soul <sornwilliam@gmail.com>
2020-04-23 18:36:41 -0500

    updated script.js with a basic format of quiz question objects.

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,6 +1,40 @@
 var timer = document.querySelector(".timer");
+var questions = document.querySelector("intro-container");
+var answers = document.querySelector("answer-btns");
+var controls = document.querySelector(".controls");
+var startbtn = document.querySelector("start-btn");
+var nextbtn = document.querySelector("next-btn");
+
+var quiz = 
+[
+    {
+        question: "1. What ?" ,
+        answer: ["A. ", "B. ", "C ", "D "]
+    },
+
+    {
+        question: "1. What ?" ,
+        answer: ["A. ", "B. ", "C ", "D "]
+    },
+
+    {
+        question: "1. What ?" ,
+        answer: ["A. ", "B. ", "C ", "D "]
+    },
+
+    {
+        question: "1. What ?" ,
+        answer: ["A. ", "B. ", "C ", "D "]
+    },
+
+    {
+        question: "1. What ?" ,
+        answer: ["A. ", "B. ", "C ", "D "]
+    },
+]
 
 var seconds = 60;
+var score = 0;
 
 function setTime(){
     var timerInterval = setInterval(function() {
@@ -14,4 +48,6 @@ function setTime(){
     }, 1000);
 }
 
-setTime();
+function startButton(){
+    
+}
